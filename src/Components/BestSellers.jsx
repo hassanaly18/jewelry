7c9138e0df315f11a0a5f223e3b969c1688e85cb
Hassan Ali <hassansiddiqui03024053707@gmail.com>
2025-03-28 01:51:44 +0500

    best sellets dynamic

--- src/Components/BestSellers.jsx
+++ src/Components/BestSellers.jsx
@@ -2,6 +2,36 @@ import React from "react";
 import Footer from "./Footer";
 
 const BestSellers = () => {
+  const data = [
+    {
+      id: 1,
+      image:
+        "https://cdn.prod.website-files.com/6683cf9f32420a5138802cae/6686273431bc1f2fbbf9f5a6_Product-Thumb-12-p-500.jpg",
+      title: "Lustrous Diamond Ring",
+      price: "$ 650.70 USD",
+    },
+    {
+      id: 2,
+      image:
+        "https://cdn.prod.website-files.com/6683cf9f32420a5138802cae/66862918d29189a265b93f17_Product-Thumb-11-p-500.jpg",
+      title: "Crystal Cascade Bracelet",
+      price: "$ 650.70 USD",
+    },
+    {
+      id: 3,
+      image:
+        "https://cdn.prod.website-files.com/6683cf9f32420a5138802cae/66862734add662916a344fa2_Product-Thumb-10-p-500.jpg",
+      title: "Radiant Ruby Chandeliers",
+      price: "$ 650.70 USD",
+    },
+    {
+      id: 4,
+      image:
+        "https://cdn.prod.website-files.com/6683cf9f32420a5138802cae/668627f0add662916a351911_Product-Thumb-09-p-500.jpg",
+      title: "Twilight Sapphire Necklace",
+      price: "$ 650.70 USD",
+    },
+  ];
 
   return (
     <div className="boxx">
@@ -11,7 +41,22 @@ const BestSellers = () => {
       </div>
       <div className="container">
         <div className="img-sec">
-          <div>
+
+          {data.map((allElements) => {
+            return (
+              <div key={allElements.id}>
+                <div className="img-ring">
+                  <img
+                    src={allElements.image}
+                    alt={allElements.title}
+                  />
+                </div>
+                <h1>{allElements.title}</h1>
+                <label>{allElements.price}</label>
+              </div>
+            );
+          })}
+          {/* <div>
             <div className="img-ring">
               <img
                 src="https://cdn.prod.website-files.com/6683cf9f32420a5138802cae/6686273431bc1f2fbbf9f5a6_Product-Thumb-12-p-500.jpg"
@@ -52,7 +97,7 @@ const BestSellers = () => {
               <h1>Twilight Sapphire Necklace</h1>
               <label>$ 650.70 USD</label>
             </div>
-          </div>
+          </div> */}
         </div>
       </div>
     </div>

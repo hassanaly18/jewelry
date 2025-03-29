1d351f4e5d71b1fc232e0b9fa60e837c135bc563
Hassan Ali <hassansiddiqui03024053707@gmail.com>
2025-03-29 16:07:38 +0500

    lesson 2 states

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Contact = () => {
-  return <div>Contact page</div>;
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [darkMode, setDarkMode] = useState(true);
+
+  return (
+    <>
+      {/* {isLoggedIn ? (
+        <h1>Welcome user!</h1>
+      ) : (
+        <h1>"Go to login page and login"</h1>
+      )}
+
+    <button onClick={() => setIsLoggedIn(!isLoggedIn)}>Login</button> */}
+
+
+      {/* Inline css toggle darkmode */}
+      <div
+        style={{ 
+          backgroundColor: darkMode ? "black" : "white",
+          color: darkMode ? "white" : "black",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+        }}
+      >
+        {/* classname changes based on darkMode state */}
+        <button className={darkMode ? "btnDark" : "btnLight"}  onClick={() => setDarkMode(!darkMode)}>
+          {darkMode ? "Light Mode" : "Dark Mode"}
+        </button>
+      </div>
+    </>
+  );
 };
 
 export default Contact;
